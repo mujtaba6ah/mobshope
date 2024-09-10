00924e6438a61d11db040469fb4e4de1cad87d12
mujtaba6ah <94188470+mujtaba6ah@users.noreply.github.com>
2024-09-10 16:39:03 -0400

    add pages and search and detail view

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,7 @@ from.import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('item/<int:id>/',views.detail,name='detail'),
 ]
 urlpatterns += static(settings.MEDIA_URL, 
                       document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from .models import Item
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     items = Item.objects.all()
+
+    #search
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        items = items.filter(name__icontains=item_name)
+
+
+
+
+    #pages
+    paginator = Paginator(items,6)
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
     return render(request,'myapp/index.html',{'items':items})
+
+def detail(request,id):
+    item = Item.objects.get(id=id)
+
+    return render(request,'myapp/detail.html',{'item':item})
